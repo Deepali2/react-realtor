0fb0688309095c3969511826000304735d3f3554
Deepali Garg <deepali.garg@zennioptical.com>
2020-02-22 00:50:02 -0800

    displays the address for every card

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardActionArea, CardMedia, CardContent } from '@material-ui/core';
+import style from './Item.css';
+
+const useStyles = makeStyles({
+  media: {
+    height: 140,
+  }
+})
 
 const Item = ({ item, activeCard, setActiveCard }) => {
+  const classes = useStyles();
+  const addressLine1 = item.address.address2 ? `${item.address.address1} ${item.address.address2}` : `${item.address.address1}`;
+  const addressLine2 = `${item.address.city}, ${item.address.state} ${item.address.zip}`;
   return (
-    <li>I am an item</li>
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={item.mainImageUrl}
+          title='image'
+          src='picture'
+        />
+        <CardContent>
+          <p>{addressLine1}</p>
+          <p>{addressLine2}</p>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   )
 }
 
