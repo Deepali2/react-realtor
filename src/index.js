d304747f1771fa10b08dc4cb8510d042af7be45b
Deepali Garg <deepali.garg@zennioptical.com>
2020-02-21 21:25:21 -0800

    sets up the webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Index = () => <div>Hello</div>;
+
+render(<Index />, document.getElementById('root'));
+
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "bundled.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:basė4",
+              sourceMap: true,
+              // minimize: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "file-loader?name=/public/icons/[name].[ext]"
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
